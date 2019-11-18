ed8aa0a080b73b2ae19c7ab3421ac656ff906502
adeleon1195 <57880266+adeleon1195@users.noreply.github.com>
2019-11-18 01:39:12 -0600

    Clase y xml para el diseñoo de la lista de peliculas y para lista de peliculas populares

--- app/src/main/java/com/example/peliculas/MainActivity.kt
+++ app/src/main/java/com/example/peliculas/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.peliculas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +11,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        MoviesRepository.getPopularMovies()
+        MoviesRepository.getPopularMovies(
+            onSuccess = ::onPopularMoviesFetched,
+            onError = ::onError
+        )
+    }
+
+    private fun onPopularMoviesFetched(movies: List<Movie>) {
+        Log.d("MainActivity", "Movies: $movies")
+    }
+
+    private fun onError() {
+        Toast.makeText(this, getString(R.string.error_fetch_movies), Toast.LENGTH_SHORT).show()
     }
 }
+
