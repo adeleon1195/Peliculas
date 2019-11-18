97567fa4aced5f6b42cfed74dfe3c97d7f0bc792
adeleon1195 <57880266+adeleon1195@users.noreply.github.com>
2019-11-18 02:16:39 -0600

    Mostrar lista de peliculas más pupulares

--- app/src/main/java/com/example/peliculas/MainActivity.kt
+++ app/src/main/java/com/example/peliculas/MainActivity.kt
@@ -4,12 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var popularMovies: RecyclerView
+    private lateinit var popularMoviesAdapter: MoviesAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        popularMovies = findViewById(R.id.popular_movies)
+        popularMovies.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        popularMoviesAdapter = MoviesAdapter(listOf())
+        popularMovies.adapter = popularMoviesAdapter
 
         MoviesRepository.getPopularMovies(
             onSuccess = ::onPopularMoviesFetched,
@@ -18,7 +32,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onPopularMoviesFetched(movies: List<Movie>) {
-        Log.d("MainActivity", "Movies: $movies")
+        popularMoviesAdapter.updateMovies(movies)
     }
 
     private fun onError() {
